ed51d4c0a804289253c2d4aa0c917731c4bc0213
Snahi <good.letterbox@gmail.com>
2020-03-24 03:15:15 +0000

    comments added in user_dao.h

--- server/user_dao.h
+++ server/user_dao.h
@@ -34,18 +34,55 @@
 
 
 
+/*
+    must be called exactly once at the beginning, before the first call to any function 
+    from this file has been done.
+    Returns:
+        INIT_USER_DAO_SUCCESS               - success
+        INIT_USER_DAO_ERR_FOLDER_CREATION   - could not create the storage folder
+        INIT_USER_DAO_ERR_MUTEX_INIT        - could not initialize the storage mutex
+*/
 int init_user_dao();
+/*
+    must be called exactly once when the functions won't be used anymore.
+    Returns:
+        DESTROY_USER_DAO_SUCCESS    - success
+        DESTROY_USER_DAO_ERR_MUTEX  - could not destroy the storage mutex
+*/
 int destroy_user_dao();
 /*
     creates a new user.
     Returns:
-        CREATE_USER_SUCCESS - success
+        CREATE_USER_SUCCESS         - success
+        CREATE_USER_ERR_EXISTS      - user with such username already exists
+        CREATE_USER_ERR_DIRECTORY   - could not create user directory
 */
-int create_user(char* name);
-int delete_user(char* name);
+int create_user(char* username);
+/*
+    deletes the user with the specified username.
+    Returns:
+        DELETE_USER_SUCCESS             - success
+        DELETE_USER_ERR_MUTEX_LOCK      - could not lock the storage mutex
+        DELETE_USER_ERR_MUTEX_UNLOCK    - could not unlock the storage mutex
+        DELETE_USER_ERR_NOT_EXISTS      - there is no user with such username
+        DELETE_USER_ERR_REMOVE_FOLDER   - could not delete the user folder
+        DELETE_USER_ERR_REMOVE_FILE     - could not delete files from user folder
+*/
+int delete_user(char* username);
+/*
+    collects names of all files of the user with the specified username in a dynamically allocated
+    array of dynamically allocated strings, so it has to be deleted afterwards. Number of the 
+    files will be put where the p_quantity points.
+    Returns:
+        GET_USER_FILES_LIST_SUCCESS             - success
+        GET_USER_FILES_LIST_ERR_NO_SUCH_USER    - there is no user with such username
+        GET_USER_FILES_LIST_ERR_MUTEX_LOCK      - could not lock the storage mutex
+        GET_USER_FILES_LIST_ERR_MUTEX_UNLOCK    - could not unlock the storage mutex
+        GET_USER_FILES_LIST_ERR_CLOSE_DIR       - could not close the user directory
+*/
 int get_user_files_list(char* username, char*** p_user_files, uint32_t* p_quantity);
 /*
-	checks if the user with the username is registered.
+	checks if the user with the specified username is registered.
 	Returns 1 if the user is registered and 0 if no
 */
 int is_registered(char* username);
